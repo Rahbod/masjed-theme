eae34a401c39dcdb7805ae300b36d939c9035ba8
Masoud <gharagozlu.masoud@gmail.com>
2020-04-23 02:51:42 +0430

    Inner pages

--- js/jquery.script.js
+++ js/jquery.script.js
@@ -158,17 +158,19 @@ $(function () {
         // });
     });
 
-    $(".content").onepage_scroll({
-        sectionContainer: "section",
-        easing: "ease",
-        animationTime: 1000,
-        pagination: true,
-        updateURL: false,
-        loop: false,
-        keyboard: true,
-        responsiveFallback: false,
-        direction: "vertical"
-    });
+    if ($.fn.onepage_scroll !== undefined) {
+        $(".content").onepage_scroll({
+            sectionContainer: "section",
+            easing: "ease",
+            animationTime: 1000,
+            pagination: true,
+            updateURL: false,
+            loop: false,
+            keyboard: true,
+            responsiveFallback: false,
+            direction: "vertical"
+        });
+    }
 
     makeHelpBox();
 });
